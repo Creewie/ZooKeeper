c2e5828326350de316e1fb9c6b829f141658c752
Creewie <tabero2006@gmail.com>
2024-12-15 11:04:05 +0100

    To nie umie pobierac danych

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 ﻿import express from 'express';
-import * as AnimalController from './controllers/AnimalsController.js';
+import * as AnimalsController from './controllers/AnimalsController.js';
 
 const app = express();
 const port= process.env.PORT || 3000;
@@ -8,7 +8,7 @@ app.get('/',(req,res)=>{
     res.send("Następne strony:<br> " +
         "<a href='http://localhost:3000/animals'>Wszystkie zwierzęta</a><br>")
 })
-app.get('/animals', AnimalController.getAllAnimals)
+app.get("/animals", AnimalsController.getAllAnimals)
 
 app.listen(port, ()=>{
     console.log("Serwer na hoście: " + port);
