80ea9132c444c442c2815ea8efa5a76b57efaba6
Creewie <tabero2006@gmail.com>
2024-12-15 09:34:46 +0100

    Na chuj tyle plikow

---
+++ app.js
@@ -0,0 +1,13 @@
+﻿import express from 'express';
+
+const app = express();
+const port= process.env.PORT || 3000;
+
+
+app.get('/', (req, res) => {
+    res.send("Działa powiedzmy")
+})
+
+app.listen(port, ()=>{
+    console.log("Serwer na hoście: " + port);
+});
